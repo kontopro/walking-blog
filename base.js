8c06560c43c73ee4f04fb9b96b0d4bb9c6c703c7
kontopro <konteap@gmail.com>
2019-01-02 01:49:12 +0200

    Update base.js

--- base.js
+++ base.js
@@ -2,9 +2,9 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyD3u34u7Hi4tkxpqVWoXJzYDjDuxNvTdDM",
-  authDomain: "playgatsby.firebaseapp.com",
-  databaseURL: "https://playgatsby.firebaseio.com"
+  apiKey: "mplamplampla",
+  authDomain: "mplamplampla.firebaseapp.com",
+  databaseURL: "https://mplamplampla.firebaseio.com"
 });
 
 const base = Rebase.createClass(firebaseApp.database());
